c6f30be25115f278896e20ff3bfd054fe3f64dc0
Alexander Kabanskiy <alexander-777@inbox.ru>
2026-06-05 18:11:46 +0300

    adding pagination

--- backend/utils/apiFilters.ts
+++ backend/utils/apiFilters.ts
@@ -20,9 +20,16 @@ class APIFilters {
 
     filter(): APIFilters {
         const queryCopy = {...this.queryStr};
-        const removeFields = ['location'];
+        const removeFields = ['location', 'page'];
         removeFields.forEach(el => delete queryCopy[el]);
         this.query = this.query.find(queryCopy);
+        return this;
+    }
+
+    pagination(resPerPage: number): APIFilters {
+        const currentPage: number = Number(this.queryStr?.page || 1);
+        const skip = (currentPage - 1) * resPerPage;
+        this.query = this.query.limit(resPerPage).skip(skip);
         return this;
     }
 }
